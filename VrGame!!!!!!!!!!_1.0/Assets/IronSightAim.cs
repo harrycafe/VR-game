5ce1cf2467aa2c10de3653541c148551bc2218b9
harrycafe <67072579+harrycafe@users.noreply.github.com>
2021-04-10 15:18:06 +0100

    Added Unity Game

---
+++ VrGame!!!!!!!!!!_1.0/Assets/IronSightAim.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+public class IronSightAim : MonoBehaviour
+{
+    public Vector3 IronSight;
+    public Vector3 HipFire;
+    public float aimSpeed = 5;
+    public GameObject Crosshair;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButton(1))
+        {
+            transform.localPosition = Vector3.Slerp(transform.localPosition,IronSight, aimSpeed * Time.deltaTime);
+            Crosshair.SetActive(false);
+        }
+        if (Input.GetMouseButtonUp(1))
+        {
+            transform.localPosition = HipFire;
+            Crosshair.SetActive(true);
+        }
+    }   
+
+}
